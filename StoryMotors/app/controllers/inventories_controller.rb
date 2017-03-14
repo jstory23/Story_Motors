45cc8f5959427f07105e1ada53c334cc1ff30300
jstory23 <jordanstory23@gmail.com>
2017-03-14 15:45:14 -0600

    updated naming structure for inventory show page

--- StoryMotors/app/controllers/inventories_controller.rb
+++ StoryMotors/app/controllers/inventories_controller.rb
@@ -29,10 +29,8 @@ class InventoriesController < ApplicationController
     respond_to do |format|
       if @inventory.save
         format.html { redirect_to @inventory, notice: 'Inventory was successfully created.' }
-        format.json { render :show, status: :created, location: @inventory }
       else
         format.html { render :new }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class InventoriesController < ApplicationController
     respond_to do |format|
       if @inventory.update(inventory_params)
         format.html { redirect_to @inventory, notice: 'Inventory was successfully updated.' }
-        format.json { render :show, status: :ok, location: @inventory }
       else
         format.html { render :edit }
-        format.json { render json: @inventory.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,7 +52,7 @@ class InventoriesController < ApplicationController
   def destroy
     @inventory.destroy
     respond_to do |format|
-      format.html { redirect_to inventories_url, notice: 'Inventory was successfully destroyed.' }
+      format.html { redirect_to inventories_url, notice: 'Inventory was removed.' }
       format.json { head :no_content }
     end
   end
